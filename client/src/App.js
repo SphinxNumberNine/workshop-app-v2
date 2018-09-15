a6c3a2d84018e026aadace5f613711f593b56f03
SphinxNumberNine <ashwathkrishnan@yahoo.com>
2018-09-15 16:46:06 -0400

    Generated front end

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        This is a test, I don't know if it worked but we'll see.
+      </div>
+    );
+  }
+}
+
+export default App;
