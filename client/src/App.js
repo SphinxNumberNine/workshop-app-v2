bddf91b2fade9e8e7652280cb9e27cc03344a011
SphinxNumberNine <ashwathkrishnan@yahoo.com>
2018-09-18 18:38:52 -0400

    Created Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import Header from './components/Header';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        This is a test, I don't know if it worked but we'll see.
-      </div>
-    );
+    return <Header />;
   }
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <nav className="red lighten-2">
+        <div className="nav-wrapper">
+          <a href="/" className="left brand-logo">
+            Sanskriti School of Dance
+          </a>
+          <ul className="right">Yerr</ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
